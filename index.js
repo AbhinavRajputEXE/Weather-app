9b2517eff37df3f133051038eb2d7355b504a189
Abhinav Rajput <80591258+AbhinavRajputEXE@users.noreply.github.com>
2023-02-21 23:02:50 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', ()=>{
+    const APIKey = '74ec9acfe98d4b49f47bd9135d855b30';
+})
